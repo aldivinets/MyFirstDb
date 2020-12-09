e2f0b77845fe1c4a1d78b1a2e0713033c2e400c4
Alexandr <alexdivinets@gmail.com>
2020-12-09 19:42:04 +0300

    Add EF
    Add EF;
    Add TeamContext

---
+++ MyFirstDb/DataModel/TeamContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MyFirstDb.DataModel
+{
+    public class TeamContext : DbContext
+    {
+        public TeamContext()
+            : base("name=TeamContext")
+        {
+        }
+    }
+
+}
