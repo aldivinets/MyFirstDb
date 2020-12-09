a7604d5a34136fa626170dedd41c58a2f59c83da
Alexandr <alexdivinets@gmail.com>
2020-12-09 19:53:22 +0300

    Add Menu
    Add menu.

--- MyFirstDb/Program.cs
+++ MyFirstDb/Program.cs
@@ -10,6 +10,24 @@ namespace MyFirstDb
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello!");
+            do
+            {
+                int choice = Menu();
+              //  Work(choice);
+            } while (true);
+        }
+        public static int Menu()
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Create;");
+            Console.WriteLine("2. Read;");
+            Console.WriteLine("3. Update;");
+            Console.WriteLine("4. Delete;");
+            Console.WriteLine("5. Exit;");
+            Console.Write("Your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            return choice;
         }
     }
 }
